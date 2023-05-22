faa70a3ece7feef6d3b18382b44d800899ab92f6
Michael1roger <michaelrogerzongo@gmail.com>
2023-05-22 12:11:05 +0000

    Docker and Github action commit

--- bostonhousepricing-main/app.py
+++ bostonhousepricing-main/app.py
@@ -5,6 +5,10 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
+######################################
+# ZONGO RAGNANGNEWENDE MICHAEL ROGER
+######################################
+
 app=Flask(__name__)
 ## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
